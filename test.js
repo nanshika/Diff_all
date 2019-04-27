bbf9d251453b66bc72fb4deef252e83b5853a89f
Toriumi Ren <arcpace.80@gmail.com>
2019-04-28 03:55:51 +0900

    First creation

---
+++ test.js
@@ -0,0 +1,17 @@
+var this_year = new Date().getFullYear();
+var t = "a	b c";
+document.write(t+'<BR>');
+var t2 = t.replace('\t','');
+document.write(t2+'<BR>');
+document.write(t+'<BR>');
+document.write('<BR><BR><BR>');
+
+document.write(new Date().getDate() + '\n');
+for ( var date = 1; date < 32; date ++ ) {
+ var tar_year = this_year + 1 - date;
+ if ( tar_year > 2005 ) {
+  document.write(tar_year + '<BR>');
+ }
+}
+
+
